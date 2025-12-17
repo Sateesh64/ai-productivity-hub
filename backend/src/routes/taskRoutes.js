312a0191f88fc0d60dfc5be7303c0a357d05985d
Sateesh64 <sateeshkethavath4@gmail.com>
2025-12-17 19:39:50 +0530

    Fix auth middleware import path

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Task = require("../models/Task");
-const auth = require("../middleware/auth");
+const auth = require("../middleware/authMiddleware");
 
 /* --------------------------------------------------
    CREATE TASK
